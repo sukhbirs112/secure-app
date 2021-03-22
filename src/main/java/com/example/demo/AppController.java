d7457fa6104963d3cb826bbc383e40cdf0701564
Sukhbir Singh <sukhbirs112@gmail.com>
2021-03-21 21:56:59 -0500

    access user in securitycontextholder

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//used for rendering data to the thymeleaf template
+import org.springframework.ui.Model;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class AppController {
@@ -16,7 +22,11 @@ public class AppController {
     }
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication auth = context.getAuthentication();
+        String username = auth.getName();
+        model.addAttribute("username", username);
         return "hello";
     }
 
